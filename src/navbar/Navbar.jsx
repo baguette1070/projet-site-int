1e91ecb82c2275bdb8e0edf8cdbf739bb33c6ce2
Houdaifa Lamrini <houdaifalamrini@MacBook-Air-de-Houdaifa.local>
2024-08-14 20:35:07 +0200

    test

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,29 +1,34 @@
+import reactLogo from '../assets/react.svg';
 
-function Navbar(){
+function Navbar() {
+    return (
+        <>
+        <div>
+            <div className='flex flex-row'>
+                <div className='pt-10 pl-10'>
+                    <img src={reactLogo} alt="" />
+                </div>
+                <h1 className=' translate-y-60 text-9xl'> Giacca</h1>
+                <div className='translate-y-96 -translate-x-72 w-1/4'>
+                    <button className='border p-4'>Commencer</button>
+                    <button className='border p-4'>Commencer</button>
+                </div>
 
-    return(<>
-        <div className="flex justify-center my-10">
-            <nav className="w-3/4 border-2 p-4 bg-vert border-vert rounded-3xl h-1/2 ">
-                <ul className="flex justify-evenly">
+                <div className='bg-gray-200 w-1/2 h-screen ml-auto'>
+                    <nav>
+                        <ul className='flex justify-around'>
+                            <li className='pt-10'><a href="">Home</a></li>
+                            <li className='pt-10'><a href="">Contact</a></li>
+                            <li className='pt-10'><a href="">About</a></li>
+                            <li className='pt-10'><a href="">Services</a></li>
+                        </ul>
+                    </nav>
 
-                    <li className="text-2xl transform transition-transform duration-200 hover:scale-125 hover:underline hover:text-bleuFonce">
-                        <a href="#">Menu</a>
-                    </li>
-                    <li className="text-2xl transform transition-transform duration-200 hover:scale-125 hover:underline hover:text-bleuFonce">
-                        <a href="#">Contact</a>
-                    </li>
-                    <li className="text-2xl transform transition-transform duration-200 hover:scale-125 hover:underline hover:text-bleuFonce">
-                        <a href="#">Contact</a>
-                    </li>
-                    <li className="text-2xl transform transition-transform duration-200 hover:scale-125 hover:underline hover:text-bleuFonce">
-                        <a href="#">Contact</a>
-                    </li>
-
-                </ul>
-            </nav>
-        </div>
-        </>);
-
+                </div>
+            </div>
+        </div>      
+        </>
+    );
 }
 
-export default Navbar
+export default Navbar;
